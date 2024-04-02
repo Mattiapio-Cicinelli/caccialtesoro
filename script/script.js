d6668c1e2db7ecf1f80002cb12eb4eadd0e09d53
Mattiapio-Cicinelli <162124916+Mattiapio-Cicinelli@users.noreply.github.com>
2024-04-02 21:27:42 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 function doLogin() {
-  var username = document.getElementById("Usarname").value;
+  var username = document.getElementById("Username").value;
   var psw = document.getElementById("Password").value;
   if(username == "" || psw == ""){
       alert("Username e Password obbligatori");
