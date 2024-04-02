801160d70d62a0b68ae622e3562add6e67fb06c8
Mattiapio-Cicinelli <162124916+Mattiapio-Cicinelli@users.noreply.github.com>
2024-04-02 11:29:14 +0200

    Update script.js
    function doLogin

--- script/script.js
+++ script/script.js
@@ -1,2 +1,8 @@
 
- console.log("messaggio");
+function doLogin() {
+  var username = document.getElementById("Usarname").value;
+  var psw = document.getElementById("Password").value;
+  if(username == "" || psw == ""){
+      alert("Username e Password obbligatori");
+  }
+}
